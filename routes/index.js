7c732c68fbfd218334694be2ab10ec0d2750d5f9
kelsonic <kgadams93@gmail.com>
2016-06-25 19:05:56 -0700

    Abstract the call to db

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var findAlgLang = require('../public/javascripts/findAlgLang');
 
 /* GET homepage. */
 router.get('/', function(req, res) {
-  var db = req.db;
-  var collection = db.get('algorithms');
-  var pickedLang = req.query.lang.toLowerCase();
-  var pickedAlg = req.query.alg.toLowerCase();
+  
+  if (req.query.length > 2 || req.query.lang === undefined || req.query.alg === undefined) {
+    res.redirect('/?lang=ruby&alg=bubble');
+  }
+  else {
+    var db = req.db;
+    var collection = db.get('algorithms');
+    var pickedLang = req.query.lang.toLowerCase();
+    var pickedAlg = req.query.alg.toLowerCase();
 
-  collection.findOne({ "lang": pickedLang }, {}, function(event, docs) {
-    var algInfo = {
-      "algorithm": docs.alg[pickedAlg].algorithm,
-      "language": docs.lang,
-      "space": docs.alg[pickedAlg].space,
-      "time": docs.alg[pickedAlg].time
-    };
-
-    res.render('index', algInfo);
-  });
+    findAlgLang(collection, pickedAlg, pickedLang, res);
+  }
 });
 
 module.exports = router;
