ef083901c870bf4e76aa080424238b7f4ae18dab
kelsonic <kgadams93@gmail.com>
2016-06-25 19:39:58 -0700

    Add private queryDatabase function

--- public/javascripts/findAlgLang.js
+++ public/javascripts/findAlgLang.js
@@ -1,7 +1,21 @@
-module.exports = function(collection, pickedAlg, pickedLang, res) {
+module.exports = function(req, res) {
+  if (req.query.length > 2 || req.query.lang === undefined || req.query.alg === undefined) {
+    res.redirect('/?lang=ruby&alg=bubble');
+  }
+  else {
+    queryDatabase(req, res);
+  }
+}
+
+function queryDatabase(req, res) {
+  
+  var db = req.db;
+  var collection = db.get('algorithms');
+  var pickedLang = req.query.lang.toLowerCase();
+  var pickedAlg = req.query.alg.toLowerCase();
+
   collection.findOne({ "lang": pickedLang }, {}, function(event, docs) {
 
-    console.log(collection);
     // If algorithm/language is not found in database
     if (docs === null || docs.alg[pickedAlg] === undefined) {
       res.status(404).send('Could not find ' + pickedAlg + ' or ' + pickedLang + ' in database.');
