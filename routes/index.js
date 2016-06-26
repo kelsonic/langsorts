2a47bcea9dcb013f51a5da4ec3239592229482b4
kelsonic <kgadams93@gmail.com>
2016-06-25 19:40:17 -0700

    Abstraction with findAlgLang module

--- routes/index.js
+++ routes/index.js
@@ -4,18 +4,7 @@ var findAlgLang = require('../public/javascripts/findAlgLang');
 
 /* GET homepage. */
 router.get('/', function(req, res) {
-  
-  if (req.query.length > 2 || req.query.lang === undefined || req.query.alg === undefined) {
-    res.redirect('/?lang=ruby&alg=bubble');
-  }
-  else {
-    var db = req.db;
-    var collection = db.get('algorithms');
-    var pickedLang = req.query.lang.toLowerCase();
-    var pickedAlg = req.query.alg.toLowerCase();
-
-    findAlgLang(collection, pickedAlg, pickedLang, res);
-  }
+  findAlgLang(req, res);
 });
 
 module.exports = router;
