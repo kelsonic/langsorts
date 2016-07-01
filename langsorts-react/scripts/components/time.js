069f31334a79ea5df7b41694e0e6967d4228f77d
kelsonic <kgadams93@gmail.com>
2016-07-01 15:07:33 -0700

    Update time component to receive props from api call

--- langsorts-react/scripts/components/time.js
+++ langsorts-react/scripts/components/time.js
@@ -3,11 +3,6 @@ import React, {Component} from 'react';
 class Time extends Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      best: "O(n)",
-      worst: "O(2^n)"
-    };
   }
 
   render() {
@@ -15,8 +10,8 @@ class Time extends Component {
       <div className="col s12 m6">
         <h4 className="orange-text">Time</h4>
         <div className="divider"></div>
-        <p className="flow-text red-text">Best: <span className="black-text">{this.state.best}</span></p>
-        <p className="flow-text red-text">Worst: <span className="black-text">{this.state.worst}</span></p>
+        <p className="flow-text red-text">Best: <span className="black-text">{this.props.best}</span></p>
+        <p className="flow-text red-text">Worst: <span className="black-text">{this.props.worst}</span></p>
       </div>
     );
   }
