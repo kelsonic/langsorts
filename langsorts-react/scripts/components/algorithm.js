55ce1ca43e64fa596e48d645e2bdd4e79bd75e71
kelsonic <kgadams93@gmail.com>
2016-07-01 15:06:37 -0700

    Update algorithm component to receive props from api call

--- langsorts-react/scripts/components/algorithm.js
+++ langsorts-react/scripts/components/algorithm.js
@@ -4,11 +4,6 @@ class Algorithm extends Component {
 
   constructor(props) {
     super(props);
-
-    this.state = { 
-      algorithm: 'Loading algorithm ...',
-      language: "ruby"
-    };
   }
 
   render() {
@@ -16,7 +11,8 @@ class Algorithm extends Component {
       <div className="row">
         <div className="col s12 m10 offset-m1 l8 offset-l2">
           <pre className="z-depth-3">
-            <code className={this.state.language}>{this.state.algorithm}
+            <code className={this.props.language}>
+              {this.props.algorithm}
             </code>
           </pre>
         </div>
