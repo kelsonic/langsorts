f46179674d6f848eaa2c03e8e59d757e2f887a37
kelsonic <kgadams93@gmail.com>
2016-07-01 15:05:51 -0700

    Inherit from algorithm parent

--- langsorts-react/scripts/App.js
+++ langsorts-react/scripts/App.js
@@ -1,28 +1,14 @@
 import React, {Component} from 'react';
-import AlgList from './components/algList';
-import Algorithm from './components/algorithm';
-import LangList from './components/langList';
-import Space from './components/space';
-import Time from './components/time';
+import AlgorithmParent from './components/algorithmParent';
 
-export default class App extends Component {
+class App extends Component {
   render() {
     return (
       <div>
-        <form action="/" method="GET" className="row">
-          <AlgList />
-          <LangList />
-        </form>
-        <Algorithm />
-        
-        <div className="row stats center">
-          <h2 className="orange-text text-darken-3">
-            Complexities
-          </h2>
-          <Space />
-          <Time />
-        </div>
+        <AlgorithmParent />
       </div>
     );
   }
 }
+
+export default App;
