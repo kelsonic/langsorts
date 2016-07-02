9c58ad1d863755b3376aa77a0c9c9b1effea85a3
kelsonic <kgadams93@gmail.com>
2016-07-01 18:42:36 -0700

    Change best case to blue-text

--- langsorts-react/scripts/components/time.js
+++ langsorts-react/scripts/components/time.js
@@ -10,7 +10,7 @@ class Time extends Component {
       <div className="col s12 m6">
         <h4 className="orange-text">Time</h4>
         <div className="divider"></div>
-        <p className="flow-text red-text">Best: <span className="black-text">{this.props.best}</span></p>
+        <p className="flow-text blue-text">Best: <span className="black-text">{this.props.best}</span></p>
         <p className="flow-text red-text">Worst: <span className="black-text">{this.props.worst}</span></p>
       </div>
     );
