e34833643f811d4e2e13d983272299eacdc8baef
kelsonic <kgadams93@gmail.com>
2016-07-01 18:00:59 -0700

    Change langList to a class with handleChange function

--- langsorts-react/scripts/components/langList.js
+++ langsorts-react/scripts/components/langList.js
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const LangList = (props) => {
+class LangList extends Component {
 
-  return(
-    <div className="col s12 m6 l4">
-      <h2 className="center orange-text text-darken-3">Languages</h2>
-      <div className="col s6 l4 offset-l2">
-        <p><input name="lang" type="radio" id="ruby" defaultChecked value="ruby" /><label htmlFor="ruby" className="red-text">Ruby</label></p>
+  constructor(props) {
+    super(props);
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.props.changeLang(e.target.value);
+  }
+
+  render() {
+    return(
+      <div className="col s12 m6 l4">
+        <h2 className="center orange-text text-darken-3">Languages</h2>
+        <div className="col s6 l4 offset-l2">
+          <p><input onChange={this.handleChange} name="lang" type="radio" id="ruby" defaultChecked value="ruby" /><label htmlFor="ruby" className="red-text">Ruby</label></p>
+        </div>
+        <div className="col s6 l4">
+          <p><input onChange={this.handleChange} name="lang" type="radio" id="javascript" value="javascript" /><label htmlFor="javascript" className="orange-text">JavaScript</label></p>
+        </div>
       </div>
-      <div className="col s6 l4">
-        <p><input name="lang" type="radio" id="javascript" value="javascript" /><label htmlFor="javascript" className="orange-text">JavaScript</label></p>
-      </div>
-    </div>
-  );
-
+    );
+  }
 }
 
 export default LangList;
